3a6e034a350284634afaa66e7d680cf19624deb9
Sukrut Rao <sukrut.rao@mpi-inf.mpg.de>
2023-11-01 09:52:05 +0100

    Fix weights download script, resolves #2

--- weights/download.sh
+++ weights/download.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-mkdir xdnn
+mkdir -p xdnn
 cd xdnn
 wget https://download.visinf.tu-darmstadt.de/data/2021-neurips-fast-axiomatic-attribution/models/xfixup_resnet50_model_best.pth.tar
 cd ..
 
-mkdir bcos
+mkdir -p bcos
 cd bcos 
-wget https://nextcloud.mpi-klsb.mpg.de/index.php/s/3Yk6p86SBBFYSBN
+wget -O resnet_50-f46c1a4159.pth https://nextcloud.mpi-klsb.mpg.de/index.php/s/3Yk6p86SBBFYSBN/download
 cd ..
 
